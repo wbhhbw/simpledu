b4f16b06a0e2d894bb33a511ac1171e67c7833e7
wbhhbw <wbhhbw90@163.com>
2017-12-28 23:21:57 +0800

    update frontend code&add form

--- simpledu/app.py
+++ simpledu/app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
 from simpledu.config import configs  # 传入configs字典
 from simpledu.models import db, Course
+from flask_migrate import Migrate
 
 
 def create_app(config):
-    """可以根据传入的config名称，加载不同的配置
+    """APP工厂
     """
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
     # SQLAlchemy 的初始化方式改为使用 init_app
     db.init_app(app)
     register_blueprints(app)
+    Migrate(app, db)
 
     return app
 
--- simpledu/handlers/front.py
+++ simpledu/handlers/front.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from simpledu.models import Course
+from simpledu.forms import LoginForm, RegisterForm
 
 # 省略了 url_prefix参数，那么默认就是 '/'
 front = Blueprint('front', __name__)
@@ -10,3 +11,13 @@ front = Blueprint('front', __name__)
 def index():
     courses = Course.query.all()
     return render_template('index.html', courses=courses)
+
+@front.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', form=form)
+
+@front.route('/register')
+def register():
+	form = RegisterForm()
+	return render_template('register.html', form=form)
