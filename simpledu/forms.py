8afd9f5b2fe886a60acfaa6f9db5166b78b2d2a8
wbhhbw <wbhhbw90@163.com>
2018-01-03 13:19:41 +0800

    compelete course CRUD

--- simpledu/forms.py
+++ simpledu/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import Length, Email, EqualTo, Required
-from simpledu.models import db, User
+from wtforms import TextAreaField, IntegerField
+from wtforms.validators import Length, Email, EqualTo, Required, URL, NumberRange
+from simpledu.models import db, User, Course
 from wtforms import ValidationError
 
 
@@ -50,3 +51,32 @@ class LoginForm(FlaskForm):
         user = User.query.filter_by(email=self.email.data).first()
         if user and not user.check_password(field.data):
             raise ValidationError('密码错误')
+
+
+class CourseForm(FlaskForm):
+    name = StringField('课程名称', validators=[Required(), Length(5, 32)])
+    description = TextAreaField(
+        '课程简介', validators=[Required(), Length(20, 256)])
+    image_url = StringField('封面图片', validators=[Required(), URL()])
+    author_id = IntegerField(
+        '作者ID', validators=[Required(), NumberRange(min=1, message='无效的用户ID')])
+    submit = SubmitField('提交')
+
+    def validate_author_id(self, field):
+        if not User.query.get(self.author_id.data):
+            raise ValidationError('用户不存在')
+
+    def create_course(self):
+        course = Course()
+        # 使用课程表单数据填充 course对象
+        self.populate_obj(course)
+        db.session.add(course)
+        db.session.commit()
+        return course
+
+    def update_course(self, course):
+        self.populate_obj(course)
+        db.session.add(course)
+        db.session.commit()
+        return course
+
--- simpledu/handlers/admin.py
+++ simpledu/handlers/admin.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
 from simpledu.decorators import admin_required
 from flask import request, current_app
-from simpledu.models import Course
-
+from simpledu.models import Course, db
+from simpledu.forms import CourseForm
+from flask import redirect, url_for, flash
 
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -24,3 +25,37 @@ def courses():
         error_out=False
     )
     return render_template('admin/courses.html', pagination=pagination)
+
+
+@admin.route('/courses/create', methods=['GET', 'POST'])
+@admin_required
+def create_course():
+    form = CourseForm()
+    if form.validate_on_submit():
+        form.create_course()
+        flash('课程创建成功', 'success')
+        return redirect(url_for('admin.courses'))
+    return render_template('admin/create_course.html', form=form)
+
+
+@admin.route('/courses/<int:course_id>/edit', methods=['GET', 'POST'])
+@admin_required
+def edit_course(course_id):
+    course = Course.query.get_or_404(course_id)
+    # 在创建 form 对象时传入了 course 对象，这样 wtforms 就会在对应的 field 中自动填入该课程的数据。
+    form = CourseForm(obj=course)
+    if form.validate_on_submit():
+        form.update_course(course)
+        flash('课程更新成功', 'success')
+        return redirect(url_for('admin.courses'))
+    return render_template('admin/edit_course.html', form=form, course=course)
+
+
+@admin.route('/courses/<int:course_id>/delete')
+@admin_required
+def delete_course(course_id):
+    course = Course.query.get_or_404(course_id)
+    db.session.delete(course)
+    db.session.commit()
+    flash('课程删除成功', 'success')
+    return redirect(url_for('admin.courses'))
